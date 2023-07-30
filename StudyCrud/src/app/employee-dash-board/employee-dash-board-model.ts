84792e5f6013e3034380fcd8f2a51b09860bb7e1
PiyumalNipuna60 <nipuna315np@gmail.com>
2023-07-30 22:19:13 +0530

    add EmployeeModel

---
+++ StudyCrud/src/app/employee-dash-board/employee-dash-board-model.ts
@@ -0,0 +1,8 @@
+export class EmployeeModel {
+    id: number = 0;
+    firstName: string = '';
+    lastName: string = '';
+    email: string = '';
+    mobile: string = '';
+    salary: string = '';
+}
